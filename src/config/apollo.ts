f2cb2515b4229f6a64e07b17cad085b2cb27b688
Nicolás Salas <nicolas.salas@imfd.cl>
2020-06-24 00:10:51 -0400

    chore(apollo): Log queries and fix resolvers path

--- src/config/apollo.ts
+++ src/config/apollo.ts
@@ -1,14 +1,24 @@
-import { buildSchema } from 'type-graphql'
 import { ApolloServer } from 'apollo-server-express'
+import { GraphQLRequestContext } from 'apollo-server-types'
 import { Express } from 'express'
+import { buildSchema } from 'type-graphql'
+
+const queryLoggingPlugin = {
+  requestDidStart(requestContext: GraphQLRequestContext) {
+    console.log('Request started! Query:\n' + requestContext.request.query)
+  },
+}
 
 export async function setupApollo(app: Express): Promise<void> {
   try {
     const schema = await buildSchema({
-      resolvers: [`${__dirname}/resolvers/**/*.{ts,js}`],
+      resolvers: [`${__dirname}/../resolvers/**/*.{ts,js}`],
     })
 
-    const apolloServer = new ApolloServer({ schema })
+    const apolloServer = new ApolloServer({
+      schema,
+      plugins: [queryLoggingPlugin],
+    })
     apolloServer.applyMiddleware({ app })
   } catch (err) {
     console.error('Failed to init apollo server')
